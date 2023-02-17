b05400ce4b2c50700df29a0450e47c055dc8d78d
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-16 21:31:35 -0500

    import { setContext }

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
 
 import Home from './pages/Home';
 import Profile from './pages/Profile';
