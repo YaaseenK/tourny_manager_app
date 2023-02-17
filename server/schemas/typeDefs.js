63f4108e912758c456f9fefff3cd165bcd6ee01e
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-16 22:55:53 -0500

    Update typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -14,13 +14,29 @@ const typeDefs = gql`
   }
 
   type Query {
+    user: User
     player: [Player]!
     player(profileId: ID!): Player
     ign: [IGN]!
     ign(ignId: ID!): IGN
   }
+    type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+  }
+  
+    type Auth {
+    token: ID
+    user: User
+  }
 
   type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    login(email: String!, password: String!): Auth
     addPlayer(firstName: String!, lastName: String!) : Player
     addIGN(playerId: ID!, IGN: String!): Player
     removePlayer(playerId: ID!): Player
